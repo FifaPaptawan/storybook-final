26133934e1e57f1ab74b1e2238546639e04016fa
Paptawan Panyapanichkul <paptawan.tcs@gmail.com>
2020-03-18 16:08:45 +0700

    init Project

---
+++ src/components/TestList.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { WithPinnedTask } from "./Tasklist.stories"
+
+it("render pinned task at the start of the list", () => {
+    const div = document.createElement("div")
+    ReactDOM.render(<WithPinnedTask />, div);
+
+    const lastTaskInput = div.querySelector(
+        '.list-item:nth-child(1) input[value="oho"]'
+    );
+    expect(lastTaskInput).not.toBe(null);
+
+    ReactDOM.unmountComponentAtNode(div);
+});
